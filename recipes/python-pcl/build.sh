1edcd0adcdb34815f3209b96c2f00e0c5655ccab
Dan Lipsa <dan.lipsa@kitware.com>
2018-08-30 21:48:06 -0400

    Compile on macos.

--- recipes/python-pcl/build.sh
+++ recipes/python-pcl/build.sh
@@ -9,6 +9,22 @@ cat <<'EOF' | patch ${PREFIX}/lib/pkgconfig/pcl_features-1.8.pc
 > Requires:  pcl_common-1.8 pcl_search-1.8 pcl_kdtree-1.8 pcl_octree-1.8 pcl_filters-1.8 #pcl_2d-1.8
 EOF
 
+
+exec_prefix=${PREFIX}
+libdir=${exec_prefix}/lib
+includedir=${PREFIX}/include/c++/v1
+
+cat <<EOF > ${PREFIX}/lib/pkgconfig/clangdev.pc
+Name: clangdev
+Description: c++ library
+Version: 6.0.1
+Requires:
+Libs: -L${libdir} -lc++
+Cflags: -I${includedir}
+EOF
+
+
+
 # The '--single-version-externally-managed --record=record.txt'
 # arguments seem to be necessary to prevent setuptools from
 # complaining about missing packages (which are already covered in the
